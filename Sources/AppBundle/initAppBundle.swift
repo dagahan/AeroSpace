6af9b53092f9c5e41bc084faf1f4114b489ad75c
dagahan <nikitosu27@gmail.com>
2026-07-18 00:37:53 +0500

    Remove 3-finger swipe gesture listener

--- Sources/AppBundle/initAppBundle.swift
+++ Sources/AppBundle/initAppBundle.swift
@@ -19,7 +19,6 @@ import Foundation
 
         startUnixSocketServer()
         GlobalObserver.initObserver()
-        TrackpadGestures.start()
         Workspace.garbageCollectUnusedWorkspaces() // init workspaces
         _ = (RestoreState.savedFocusedWorkspace.map { Workspace.get(byName: $0) } ?? Workspace.all.first)?.focusWorkspace()
         await runHeavyCompleteRefreshSession(
